91c030d4a3070c5b944814bdef0b46aa822c47d6
Basil Dubyk <samitnuk.work@gmail.com>
2019-07-16 10:16:46 +0300

    Fork Oscar's `offer` app

--- apps/offer/apps.py
+++
@@ -1,7 +0,0 @@
-import oscar.apps.offer.apps as apps
-
-
-class OfferConfig(apps.OfferConfig):
-    label = 'offer'
-    name = 'apps.offer'
-    verbose_name = 'Offer'
--- flash_sale/urls.py
+++ flash_sale/urls.py
@@ -1,13 +1,14 @@
-from django.apps import apps
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from oscar.app import application
+
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
-    path('', include(apps.get_app_config('oscar').urls[0]))
+    path('', application.urls),
 ]
 
 if settings.DEBUG:
