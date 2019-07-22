76a1ce4bbd80e2b70be9ef0d2a05edeb55fa30f2
Basil Dubyk <samitnuk.work@gmail.com>
2019-07-22 13:15:07 +0300

    Rename `sale_price` method of `Product` to `get_flash_price_benefit`

--- apps/catalogue/models.py
+++ apps/catalogue/models.py
@@ -9,15 +9,14 @@ ConditionalOffer = get_model('offer', 'ConditionalOffer')
 
 class Product(AbstractProduct):
 
-    def sale_price(self):
+    def get_flash_price_benefit(self):
         if self.includes.filter(
             benefit__offers__offer_type=ConditionalOffer.SALE,
             benefit__offers__start_datetime__lt=now(),
             benefit__offers__end_datetime__gt=now(),
         ).exists():
             range_ = self.includes.first()
-            benefit = range_.benefit_set.first()
-            return benefit.calculate_sale_price(self)
+            return range_.benefit_set.first()
 
 
 from oscar.apps.catalogue.models import *  # noqa isort:skip
