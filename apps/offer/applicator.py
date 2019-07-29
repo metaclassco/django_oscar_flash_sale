7f5ddb17ddfa1d057c58759031c4a3f785e42d94
Alexander Gaevsky <sasha@sasha0.ru>
2019-07-29 17:07:14 +0300

    Distinguish sale and site offers in the offer applicator.

--- apps/offer/applicator.py
+++ apps/offer/applicator.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 from oscar.apps.offer.applicator import Applicator as CoreApplicator
 from oscar.core.loading import get_model
 
@@ -7,6 +9,20 @@ ConditionalOffer = get_model('offer', 'ConditionalOffer')
 
 class Applicator(CoreApplicator):
 
-    def get_site_offers(self):
-        qs = ConditionalOffer.active.filter(offer_type__in=[ConditionalOffer.SITE, ConditionalOffer.SALE])
+    def get_sale_offers(self):
+        qs = ConditionalOffer.active.filter(offer_type=ConditionalOffer.SALE)
         return qs.select_related('condition', 'benefit')
+
+    def get_offers(self, basket, user=None, request=None):
+        site_offers = self.get_site_offers()
+        basket_offers = self.get_basket_offers(basket, user)
+        user_offers = self.get_user_offers(user)
+        session_offers = self.get_session_offers(request)
+        sale_offers = self.get_sale_offers()
+
+        return list(
+            sorted(chain(
+                session_offers, basket_offers, user_offers, site_offers, sale_offers),
+                key=lambda o: o.priority, reverse=True
+            )
+        )
